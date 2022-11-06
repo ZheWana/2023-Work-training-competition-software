14779d8a6a941c9a148a4bf2056e38291c1a4653
ZheWana <1312560006@qq.com>
2022-11-06 10:49:44 +0800

    添加屏幕依赖; 添加测试代码

--- ControlCenter/Core/Src/freertos.c
+++ ControlCenter/Core/Src/freertos.c
@@ -27,6 +27,7 @@
 /* USER CODE BEGIN Includes */
 #include "printf.h"
 #include "usart.h"
+#include "st7735.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -51,49 +52,49 @@
 /* Definitions for LEDcontrol */
 osThreadId_t LEDcontrolHandle;
 const osThreadAttr_t LEDcontrol_attributes = {
-        .name = "LEDcontrol",
-        .stack_size = 128 * 4,
-        .priority = (osPriority_t) osPriorityHigh,
+  .name = "LEDcontrol",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
 };
 /* Definitions for ScreenRefresh */
 osThreadId_t ScreenRefreshHandle;
 const osThreadAttr_t ScreenRefresh_attributes = {
-        .name = "ScreenRefresh",
-        .stack_size = 128 * 4,
-        .priority = (osPriority_t) osPriorityHigh,
+  .name = "ScreenRefresh",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
 };
 /* Definitions for StateMachine */
 osThreadId_t StateMachineHandle;
 const osThreadAttr_t StateMachine_attributes = {
-        .name = "StateMachine",
-        .stack_size = 128 * 4,
-        .priority = (osPriority_t) osPriorityRealtime,
+  .name = "StateMachine",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityRealtime,
 };
 /* Definitions for GetInfraredData */
 osThreadId_t GetInfraredDataHandle;
 const osThreadAttr_t GetInfraredData_attributes = {
-        .name = "GetInfraredData",
-        .stack_size = 128 * 4,
-        .priority = (osPriority_t) osPriorityNormal,
+  .name = "GetInfraredData",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
 };
 /* Definitions for StepControl */
 osThreadId_t StepControlHandle;
 const osThreadAttr_t StepControl_attributes = {
-        .name = "StepControl",
-        .stack_size = 128 * 4,
-        .priority = (osPriority_t) osPriorityHigh,
+  .name = "StepControl",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
 };
 /* Definitions for SerialOutput */
 osThreadId_t SerialOutputHandle;
 const osThreadAttr_t SerialOutput_attributes = {
-        .name = "SerialOutput",
-        .stack_size = 128 * 4,
-        .priority = (osPriority_t) osPriorityLow,
+  .name = "SerialOutput",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
 };
 /* Definitions for MessageQueue */
 osMessageQueueId_t MessageQueueHandle;
 const osMessageQueueAttr_t MessageQueue_attributes = {
-        .name = "MessageQueue"
+  .name = "MessageQueue"
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -105,15 +106,10 @@ void _putchar(char character) {
 /* USER CODE END FunctionPrototypes */
 
 void ledControlEnter(void *argument);
-
 void ScreenRefreshEntry(void *argument);
-
 void StateMachineEntry(void *argument);
-
 void GetInfraredDataEntry(void *argument);
-
 void StepControlEntry(void *argument);
-
 void SerialOutputEntry(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
@@ -124,56 +120,56 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
   * @retval None
   */
 void MX_FREERTOS_Init(void) {
-    /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-    /* USER CODE END Init */
+  /* USER CODE END Init */
 
-    /* USER CODE BEGIN RTOS_MUTEX */
+  /* USER CODE BEGIN RTOS_MUTEX */
     /* add mutexes, ... */
-    /* USER CODE END RTOS_MUTEX */
+  /* USER CODE END RTOS_MUTEX */
 
-    /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
     /* add semaphores, ... */
-    /* USER CODE END RTOS_SEMAPHORES */
+  /* USER CODE END RTOS_SEMAPHORES */
 
-    /* USER CODE BEGIN RTOS_TIMERS */
+  /* USER CODE BEGIN RTOS_TIMERS */
     /* start timers, add new ones, ... */
-    /* USER CODE END RTOS_TIMERS */
+  /* USER CODE END RTOS_TIMERS */
 
-    /* Create the queue(s) */
-    /* creation of MessageQueue */
-    MessageQueueHandle = osMessageQueueNew(16, sizeof(uint32_t), &MessageQueue_attributes);
+  /* Create the queue(s) */
+  /* creation of MessageQueue */
+  MessageQueueHandle = osMessageQueueNew (16, sizeof(uint32_t), &MessageQueue_attributes);
 
-    /* USER CODE BEGIN RTOS_QUEUES */
+  /* USER CODE BEGIN RTOS_QUEUES */
     /* add queues, ... */
-    /* USER CODE END RTOS_QUEUES */
+  /* USER CODE END RTOS_QUEUES */
 
-    /* Create the thread(s) */
-    /* creation of LEDcontrol */
-    LEDcontrolHandle = osThreadNew(ledControlEnter, NULL, &LEDcontrol_attributes);
+  /* Create the thread(s) */
+  /* creation of LEDcontrol */
+  LEDcontrolHandle = osThreadNew(ledControlEnter, NULL, &LEDcontrol_attributes);
 
-    /* creation of ScreenRefresh */
-    ScreenRefreshHandle = osThreadNew(ScreenRefreshEntry, NULL, &ScreenRefresh_attributes);
+  /* creation of ScreenRefresh */
+  ScreenRefreshHandle = osThreadNew(ScreenRefreshEntry, NULL, &ScreenRefresh_attributes);
 
-    /* creation of StateMachine */
-    StateMachineHandle = osThreadNew(StateMachineEntry, NULL, &StateMachine_attributes);
+  /* creation of StateMachine */
+  StateMachineHandle = osThreadNew(StateMachineEntry, NULL, &StateMachine_attributes);
 
-    /* creation of GetInfraredData */
-    GetInfraredDataHandle = osThreadNew(GetInfraredDataEntry, NULL, &GetInfraredData_attributes);
+  /* creation of GetInfraredData */
+  GetInfraredDataHandle = osThreadNew(GetInfraredDataEntry, NULL, &GetInfraredData_attributes);
 
-    /* creation of StepControl */
-    StepControlHandle = osThreadNew(StepControlEntry, NULL, &StepControl_attributes);
+  /* creation of StepControl */
+  StepControlHandle = osThreadNew(StepControlEntry, NULL, &StepControl_attributes);
 
-    /* creation of SerialOutput */
-    SerialOutputHandle = osThreadNew(SerialOutputEntry, NULL, &SerialOutput_attributes);
+  /* creation of SerialOutput */
+  SerialOutputHandle = osThreadNew(SerialOutputEntry, NULL, &SerialOutput_attributes);
 
-    /* USER CODE BEGIN RTOS_THREADS */
+  /* USER CODE BEGIN RTOS_THREADS */
     /* add threads, ... */
-    /* USER CODE END RTOS_THREADS */
+  /* USER CODE END RTOS_THREADS */
 
-    /* USER CODE BEGIN RTOS_EVENTS */
+  /* USER CODE BEGIN RTOS_EVENTS */
     /* add events, ... */
-    /* USER CODE END RTOS_EVENTS */
+  /* USER CODE END RTOS_EVENTS */
 
 }
 
@@ -184,8 +180,9 @@ void MX_FREERTOS_Init(void) {
   * @retval None
   */
 /* USER CODE END Header_ledControlEnter */
-void ledControlEnter(void *argument) {
-    /* USER CODE BEGIN ledControlEnter */
+void ledControlEnter(void *argument)
+{
+  /* USER CODE BEGIN ledControlEnter */
     /* Infinite loop */
     for (;;) {
         HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
@@ -193,7 +190,7 @@ void ledControlEnter(void *argument) {
         HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
         osDelay(950);
     }
-    /* USER CODE END ledControlEnter */
+  /* USER CODE END ledControlEnter */
 }
 
 /* USER CODE BEGIN Header_ScreenRefreshEntry */
@@ -203,13 +200,14 @@ void ledControlEnter(void *argument) {
 * @retval None
 */
 /* USER CODE END Header_ScreenRefreshEntry */
-void ScreenRefreshEntry(void *argument) {
-    /* USER CODE BEGIN ScreenRefreshEntry */
+void ScreenRefreshEntry(void *argument)
+{
+  /* USER CODE BEGIN ScreenRefreshEntry */
     /* Infinite loop */
     for (;;) {
         osDelay(1);
     }
-    /* USER CODE END ScreenRefreshEntry */
+  /* USER CODE END ScreenRefreshEntry */
 }
 
 /* USER CODE BEGIN Header_StateMachineEntry */
@@ -219,13 +217,14 @@ void ScreenRefreshEntry(void *argument) {
 * @retval None
 */
 /* USER CODE END Header_StateMachineEntry */
-void StateMachineEntry(void *argument) {
-    /* USER CODE BEGIN StateMachineEntry */
+void StateMachineEntry(void *argument)
+{
+  /* USER CODE BEGIN StateMachineEntry */
     /* Infinite loop */
     for (;;) {
         osDelay(1);
     }
-    /* USER CODE END StateMachineEntry */
+  /* USER CODE END StateMachineEntry */
 }
 
 /* USER CODE BEGIN Header_GetInfraredDataEntry */
@@ -235,13 +234,14 @@ void StateMachineEntry(void *argument) {
 * @retval None
 */
 /* USER CODE END Header_GetInfraredDataEntry */
-void GetInfraredDataEntry(void *argument) {
-    /* USER CODE BEGIN GetInfraredDataEntry */
+void GetInfraredDataEntry(void *argument)
+{
+  /* USER CODE BEGIN GetInfraredDataEntry */
     /* Infinite loop */
     for (;;) {
         osDelay(1);
     }
-    /* USER CODE END GetInfraredDataEntry */
+  /* USER CODE END GetInfraredDataEntry */
 }
 
 /* USER CODE BEGIN Header_StepControlEntry */
@@ -251,13 +251,17 @@ void GetInfraredDataEntry(void *argument) {
 * @retval None
 */
 /* USER CODE END Header_StepControlEntry */
-void StepControlEntry(void *argument) {
-    /* USER CODE BEGIN StepControlEntry */
+void StepControlEntry(void *argument)
+{
+  /* USER CODE BEGIN StepControlEntry */
     /* Infinite loop */
     for (;;) {
-        osDelay(1);
+        char *string = "message";
+        size_t buff = (size_t) string;
+        osMessageQueuePut(MessageQueueHandle, &buff, 0, 0xffff);
+        osDelay(200);
     }
-    /* USER CODE END StepControlEntry */
+  /* USER CODE END StepControlEntry */
 }
 
 /* USER CODE BEGIN Header_SerialOutputEntry */
@@ -267,15 +271,17 @@ void StepControlEntry(void *argument) {
 * @retval None
 */
 /* USER CODE END Header_SerialOutputEntry */
-void SerialOutputEntry(void *argument) {
-    /* USER CODE BEGIN SerialOutputEntry */
+void SerialOutputEntry(void *argument)
+{
+  /* USER CODE BEGIN SerialOutputEntry */
     /* Infinite loop */
     for (;;) {
-        static int a = 0;
-        printf("%d\n", a++);
+        size_t msg;
+        osMessageQueueGet(MessageQueueHandle, &msg, 0, 0xffff);
+        printf("%s\n", (char *) msg);
         osDelay(100);
     }
-    /* USER CODE END SerialOutputEntry */
+  /* USER CODE END SerialOutputEntry */
 }
 
 /* Private application code --------------------------------------------------*/
