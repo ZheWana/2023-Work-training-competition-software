81292b0c391e6df0976d99fa2f86cb9b031b7d04
ZheWana <1312560006@qq.com>
2022-11-12 15:07:31 +0800

    调节QMC5883读取代码

--- ControlCenter/UserCode/Compass/QMC5883L.c
+++ ControlCenter/UserCode/Compass/QMC5883L.c
@@ -14,22 +14,30 @@ int QMC5883_Init(void) {
         return -1;
     if (check == 0xFF) {
         data = 0x01;
-        if (HAL_I2C_Mem_Read(&hi2c1, QMC5883_Addr, QMC_SETorRESETReg, 1, (uint8_t *) &data, 1, 1000))
+        if (HAL_I2C_Mem_Write(&hi2c1, QMC5883_Addr, QMC_SETorRESETReg, 1, (uint8_t *) &data, 1, 1000))
             return -1;
-        //配置持续测量模式
-        data = 0x1D;
+        data = 0x15;
+        if (HAL_I2C_Mem_Write(&hi2c1, QMC5883_Addr, QMC_ControlReg1, 1, (uint8_t *) &data, 1, 1000))
+            return -1;
+        uint8_t temp = data;
         if (HAL_I2C_Mem_Read(&hi2c1, QMC5883_Addr, QMC_ControlReg1, 1, (uint8_t *) &data, 1, 1000))
             return -1;
+        if (data != temp)return -3;
     } else { return -2; }
     return 0;
 }
 
 int QMC5883_GetData(hmcData_t *data) {
-    uint8_t temp[6];
-    if (HAL_I2C_Mem_Read(&hi2c1, QMC5883_Addr, QMC_DataXLSB, 1, (uint8_t *) &temp, 6, 0xffff))
+    uint8_t status;
+    if (HAL_I2C_Mem_Read(&hi2c1, QMC5883_Addr, QMC_DataXLSB, 1, (uint8_t *) &status, 1, HAL_MAX_DELAY))
         return -1;
-    data->Mx = (float) (int16_t) (temp[0] << 8 | temp[1]);
-    data->Mz = (float) (int16_t) (temp[2] << 8 | temp[3]);
-    data->My = (float) (int16_t) (temp[4] << 8 | temp[5]);
+    if ((status & 1) != 1)return -2;
+
+    uint8_t temp[6];
+    if (HAL_I2C_Mem_Read(&hi2c1, QMC5883_Addr, QMC_DataXLSB, 1, (uint8_t *) &temp, 6, HAL_MAX_DELAY))
+        return -1;
+    data->Mx = (float) (int16_t) (temp[1] << 8 | temp[0]);
+    data->Mz = (float) (int16_t) (temp[3] << 8 | temp[2]);
+    data->My = (float) (int16_t) (temp[5] << 8 | temp[4]);
     return 0;
 }
