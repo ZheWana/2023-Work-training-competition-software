2b42f1358a8cfc29d894a6cd14848e96d53b9644
ZheWana <1312560006@qq.com>
2023-01-28 12:57:08 +0800

    重构部分函数名称，添加红外传感器数据读取

--- Controler/Core/Src/freertos.c
+++ Controler/Core/Src/freertos.c
@@ -28,6 +28,7 @@
 #include "key.h"
 #include "move.h"
 #include "stdio.h"
+#include "float.h"
 #include "usart.h"
 #include "utils.h"
 #include "stdlib.h"
@@ -63,28 +64,35 @@
 /* Definitions for LEDcontrol */
 osThreadId_t LEDcontrolHandle;
 const osThreadAttr_t LEDcontrol_attributes = {
-  .name = "LEDcontrol",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
+        .name = "LEDcontrol",
+        .stack_size = 128 * 4,
+        .priority = (osPriority_t) osPriorityLow,
 };
 /* Definitions for SerialOutput */
 osThreadId_t SerialOutputHandle;
 const osThreadAttr_t SerialOutput_attributes = {
-  .name = "SerialOutput",
-  .stack_size = 512 * 4,
-  .priority = (osPriority_t) osPriorityLow,
+        .name = "SerialOutput",
+        .stack_size = 512 * 4,
+        .priority = (osPriority_t) osPriorityLow,
 };
 /* Definitions for StateMachine */
 osThreadId_t StateMachineHandle;
 const osThreadAttr_t StateMachine_attributes = {
-  .name = "StateMachine",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
+        .name = "StateMachine",
+        .stack_size = 128 * 4,
+        .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for ScreenRefresh */
+osThreadId_t ScreenRefreshHandle;
+const osThreadAttr_t ScreenRefresh_attributes = {
+        .name = "ScreenRefresh",
+        .stack_size = 512 * 4,
+        .priority = (osPriority_t) osPriorityLow,
 };
 /* Definitions for bQueuePut */
 osSemaphoreId_t bQueuePutHandle;
 const osSemaphoreAttr_t bQueuePut_attributes = {
-  .name = "bQueuePut"
+        .name = "bQueuePut"
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -93,8 +101,12 @@ const osSemaphoreAttr_t bQueuePut_attributes = {
 /* USER CODE END FunctionPrototypes */
 
 void LEDcontrolEntry(void *argument);
+
 void SerialOutputEntry(void *argument);
+
 void StateMachineEntry(void *argument);
+
+void ScreenRefreshEntry(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -104,47 +116,50 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
   * @retval None
   */
 void MX_FREERTOS_Init(void) {
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* USER CODE BEGIN RTOS_MUTEX */
+    /* USER CODE BEGIN RTOS_MUTEX */
     /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
+    /* USER CODE END RTOS_MUTEX */
 
-  /* Create the semaphores(s) */
-  /* creation of bQueuePut */
-  bQueuePutHandle = osSemaphoreNew(1, 1, &bQueuePut_attributes);
+    /* Create the semaphores(s) */
+    /* creation of bQueuePut */
+    bQueuePutHandle = osSemaphoreNew(1, 1, &bQueuePut_attributes);
 
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
     /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
+    /* USER CODE END RTOS_SEMAPHORES */
 
-  /* USER CODE BEGIN RTOS_TIMERS */
+    /* USER CODE BEGIN RTOS_TIMERS */
     /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
+    /* USER CODE END RTOS_TIMERS */
 
-  /* USER CODE BEGIN RTOS_QUEUES */
+    /* USER CODE BEGIN RTOS_QUEUES */
     /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
+    /* USER CODE END RTOS_QUEUES */
 
-  /* Create the thread(s) */
-  /* creation of LEDcontrol */
-  LEDcontrolHandle = osThreadNew(LEDcontrolEntry, NULL, &LEDcontrol_attributes);
+    /* Create the thread(s) */
+    /* creation of LEDcontrol */
+    LEDcontrolHandle = osThreadNew(LEDcontrolEntry, NULL, &LEDcontrol_attributes);
 
-  /* creation of SerialOutput */
-  SerialOutputHandle = osThreadNew(SerialOutputEntry, NULL, &SerialOutput_attributes);
+    /* creation of SerialOutput */
+    SerialOutputHandle = osThreadNew(SerialOutputEntry, NULL, &SerialOutput_attributes);
 
-  /* creation of StateMachine */
-  StateMachineHandle = osThreadNew(StateMachineEntry, NULL, &StateMachine_attributes);
+    /* creation of StateMachine */
+    StateMachineHandle = osThreadNew(StateMachineEntry, NULL, &StateMachine_attributes);
 
-  /* USER CODE BEGIN RTOS_THREADS */
+    /* creation of ScreenRefresh */
+    ScreenRefreshHandle = osThreadNew(ScreenRefreshEntry, NULL, &ScreenRefresh_attributes);
+
+    /* USER CODE BEGIN RTOS_THREADS */
     /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
+    /* USER CODE END RTOS_THREADS */
 
-  /* USER CODE BEGIN RTOS_EVENTS */
+    /* USER CODE BEGIN RTOS_EVENTS */
     /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
+    /* USER CODE END RTOS_EVENTS */
 
 }
 
@@ -155,9 +170,8 @@ void MX_FREERTOS_Init(void) {
   * @retval None
   */
 /* USER CODE END Header_LEDcontrolEntry */
-void LEDcontrolEntry(void *argument)
-{
-  /* USER CODE BEGIN LEDcontrolEntry */
+void LEDcontrolEntry(void *argument) {
+    /* USER CODE BEGIN LEDcontrolEntry */
     /* Infinite loop */
     for (;;) {
         HAL_GPIO_WritePin(LED_System_GPIO_Port, LED_System_Pin, GPIO_PIN_SET);
@@ -165,7 +179,7 @@ void LEDcontrolEntry(void *argument)
         HAL_GPIO_WritePin(LED_System_GPIO_Port, LED_System_Pin, GPIO_PIN_RESET);
         osDelay(950);
     }
-  /* USER CODE END LEDcontrolEntry */
+    /* USER CODE END LEDcontrolEntry */
 }
 
 /* USER CODE BEGIN Header_SerialOutputEntry */
@@ -175,20 +189,21 @@ void LEDcontrolEntry(void *argument)
 * @retval None
 */
 /* USER CODE END Header_SerialOutputEntry */
-void SerialOutputEntry(void *argument)
-{
-  /* USER CODE BEGIN SerialOutputEntry */
+void SerialOutputEntry(void *argument) {
+    /* USER CODE BEGIN SerialOutputEntry */
     /* Infinite loop */
     for (;;) {
 //        printf("data:");
 //        printf("%d,", CarInfo.spd[0]);
-//        printf("%f,", CarInfo.mPid[0].ctr.aim);
+//        printf("%f,", CarInfo.msPid[0].ctr.aim);
 //        printf("%f,", CarInfo.psi[0]);
-//        printf("%f,", CarInfo.pPid[0].ctr.aim);
-////        printf("%f", log2(CarInfo.isCarMoving == 0 ? 1 : CarInfo.isCarMoving));
+//        printf("%f,", CarInfo.mpPid[0].ctr.aim);
+//        printf("%f,", CarInfo.hmc.Mx);
+//        printf("%f,", CarInfo.hmc.My);
+//        printf("%f,", ToDig(CarInfo.yaw));
 //        printf("\n");
     }
-  /* USER CODE END SerialOutputEntry */
+    /* USER CODE END SerialOutputEntry */
 }
 
 /* USER CODE BEGIN Header_StateMachineEntry */
@@ -198,17 +213,60 @@ void SerialOutputEntry(void *argument)
 * @retval None
 */
 /* USER CODE END Header_StateMachineEntry */
-void StateMachineEntry(void *argument)
-{
-  /* USER CODE BEGIN StateMachineEntry */
+void StateMachineEntry(void *argument) {
+    /* USER CODE BEGIN StateMachineEntry */
     /* Infinite loop */
     for (;;) {
-        ST7735_FillScreen(ST7735_WHITE);
-        ST7735_FillScreen(ST7735_RED);
-        ST7735_FillScreen(ST7735_GREEN);
-        ST7735_FillScreen(ST7735_BLUE);
+//        MecanumRotate(10000000,10,1);
+        osDelay(1);
     }
-  /* USER CODE END StateMachineEntry */
+    /* USER CODE END StateMachineEntry */
+}
+
+/* USER CODE BEGIN Header_ScreenRefreshEntry */
+/**
+* @brief Function implementing the ScreenRefresh thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_ScreenRefreshEntry */
+void ScreenRefreshEntry(void *argument) {
+    /* USER CODE BEGIN ScreenRefreshEntry */
+    /* Infinite loop */
+    for (;;) {
+        char buff[64];
+        static uint32_t pretick = 0;
+        float fps = 1000.0f / (HAL_GetTick() - pretick);
+
+        pretick = HAL_GetTick();
+        sprintf(buff, "FPS:%.3f\n", fps);
+        LCD_StringLayout(128, buff, Font_7x10, ST7735_BLACK, ST7735_WHITE);
+
+//        for (int i = 0; i < 16; i++) {
+//            buff[i] = CarInfo.infrared & (1 << i) ? '1' : '0';
+//        }
+//        buff[16] = '\0';
+//        LCD_StringLayout(128, buff, Font_7x10, ST7735_BLACK, ST7735_WHITE);
+//        for (int i = 0; i < 16; i++) {
+//            buff[i] = CarInfo.infrared & (1 << (i + 16)) ? '1' : '0';
+//        }
+//        buff[16] = '\0';
+//        LCD_StringLayout(128, buff, Font_7x10, ST7735_BLACK, ST7735_WHITE);
+        sprintf(buff, CarInfo.yaw > 0 ? "Yaw: %.3f\n" : "Yaw:%.3f\n", ToDig(CarInfo.yaw));
+        LCD_StringLayout(128, buff, Font_11x18, ST7735_BLACK, ST7735_WHITE);
+        sprintf(buff, "dx:%.3f\n", CarInfo.dx);
+        LCD_StringLayout(128, buff, Font_7x10, ST7735_BLACK, ST7735_WHITE);
+        sprintf(buff, "dy:%.3f\n", CarInfo.dy);
+        LCD_StringLayout(128, buff, Font_7x10, ST7735_BLACK, ST7735_WHITE);
+        sprintf(buff, "X:%.3f\n", CarInfo.curX);
+        LCD_StringLayout(128, buff, Font_11x18, ST7735_BLACK, ST7735_WHITE);
+        sprintf(buff, "Y:%.3f\n", CarInfo.curY);
+        LCD_StringLayout(128, buff, Font_11x18, ST7735_BLACK, ST7735_WHITE);
+
+        // End of page
+        LCD_StringLayout(LCD_EOP);
+    }
+    /* USER CODE END ScreenRefreshEntry */
 }
 
 /* Private application code --------------------------------------------------*/
--- Controler/Core/Src/main.c
+++ Controler/Core/Src/main.c
@@ -33,6 +33,7 @@
 #include "motor.h"
 #include "utils.h"
 #include "PID/pid.h"
+#include "74HC165/74HC165.h"
 #include "Compass/QMC5883L.h"
 #include "ST7735-HAL/st7735.h"
 #include "ICM42605/ICM42605.h"
@@ -66,9 +67,13 @@ char chBuff;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+
 void PeriphCommonClock_Config(void);
+
 static void MPU_Config(void);
+
 void MX_FREERTOS_Init(void);
+
 /* USER CODE BEGIN PFP */
 void _putchar(char character) {
     HAL_UART_Transmit(&huart5, (uint8_t *) &character, 1, HAL_MAX_DELAY);
@@ -86,53 +91,52 @@ extern short uart_charPut(char *data, unsigned short len);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+int main(void) {
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MPU Configuration--------------------------------------------------------*/
-  MPU_Config();
+    /* MPU Configuration--------------------------------------------------------*/
+    MPU_Config();
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
 /* Configure the peripherals common clocks */
-  PeriphCommonClock_Config();
+    PeriphCommonClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_UART4_Init();
-  MX_SPI3_Init();
-  MX_UART5_Init();
-  MX_UART8_Init();
-  MX_I2C1_Init();
-  MX_DMA_Init();
-  MX_TIM2_Init();
-  MX_TIM4_Init();
-  MX_TIM16_Init();
-  MX_TIM3_Init();
-  MX_SPI4_Init();
-  MX_TIM1_Init();
-  MX_TIM8_Init();
-  MX_SPI1_Init();
-  MX_TIM15_Init();
-  MX_TIM6_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_UART4_Init();
+    MX_SPI3_Init();
+    MX_UART5_Init();
+    MX_UART8_Init();
+    MX_I2C1_Init();
+    MX_DMA_Init();
+    MX_TIM2_Init();
+    MX_TIM4_Init();
+    MX_TIM16_Init();
+    MX_TIM3_Init();
+    MX_SPI4_Init();
+    MX_TIM1_Init();
+    MX_TIM8_Init();
+    MX_SPI1_Init();
+    MX_TIM15_Init();
+    MX_TIM6_Init();
+    /* USER CODE BEGIN 2 */
     // Encoders
     HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_1);
     HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_2);
@@ -153,25 +157,31 @@ int main(void)
 
     // Screen
     ST7735_Init();
+    ST7735_SetRotation(1);
     ST7735_Backlight_On();
-
+    ST7735_FillScreen(ST7735_WHITE);
 
     // Sensor Init
-//    int res = 0;
-//    if (PMW3901_Init(&CarInfo.pmw) != 0)
-//        while (1)printf("PMW Init Error!\n");
-//    if (QMC5883_Init() != 0)
-//        while (1)printf("QMC Init Error!\n");
+    int res = 0;
+    if (PMW3901_Init(&CarInfo.pmw) != 0)
+        while (1);
+    while (QMC5883_Init() != 0);
+
+    for (int i = 0; i < 100; i++) {
+        QMC5883_GetData(&CarInfo.hmc);
+        CarInfo.initYawOffset += atan2f(CarInfo.hmc.Mx, CarInfo.hmc.My) * 0.01;
+    }
+
 //    if ((res = ICM42605_Init()) != 0)
 //        while (1)printf("ICM Init Error:%d\n", res);
 
     // Soft Init
     for (int i = 0; i < 4; i++) {
-        PID_Init(&CarInfo.mPid[i], 10, 0.5f, 0);//0, 0.2, 0,
-        PID_Init(&CarInfo.pPid[i], 0.15f, 0, 0);// 0.033, 0, 0,
+        PID_Init(&CarInfo.msPid[i], 10, 0.5f, 0);//0, 0.2, 0,
+        PID_Init(&CarInfo.mpPid[i], 0.15f, 0, 0);// 0.033, 0, 0,
     }
-//    PID_Init(&CarInfo.mPid[0], 10, 0.5f, 0);//0, 0.2, 0,
-//    PID_Init(&CarInfo.pPid[0], 0.15f, 0, 0);// 0.033, 0, 0,
+//    PID_Init(&CarInfo.msPid[0], 10, 0.5f, 0);//0, 0.2, 0,
+//    PID_Init(&CarInfo.mpPid[0], 0.15f, 0, 0);// 0.033, 0, 0,
 
     shell.write = uart_charPut;
     shellInit(&shell, shBuff, 128);
@@ -179,106 +189,101 @@ int main(void)
     // Interrupt
     HAL_TIM_Base_Start_IT(&htim6);
     HAL_UART_Receive_IT(&huart5, (uint8_t *) &chBuff, 1);
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Init scheduler */
-  osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
-  MX_FREERTOS_Init();
+    /* Init scheduler */
+    osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
+    MX_FREERTOS_Init();
 
-  /* Start scheduler */
-  osKernelStart();
+    /* Start scheduler */
+    osKernelStart();
 
-  /* We should never get here as control is now taken by the scheduler */
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* We should never get here as control is now taken by the scheduler */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1) {
         Motor_Drive(0, 20, 1);
         Motor_Drive(1, 20, 1);
         Motor_Drive(2, 20, 1);
         Motor_Drive(3, 20, 1);
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Supply configuration update enable
-  */
-  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+    /** Supply configuration update enable
+    */
+    HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 5;
-  RCC_OscInitStruct.PLL.PLLN = 192;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 3;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
-  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 5;
+    RCC_OscInitStruct.PLL.PLLN = 192;
+    RCC_OscInitStruct.PLL.PLLP = 2;
+    RCC_OscInitStruct.PLL.PLLQ = 3;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLFRACN = 0;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
+                                  | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+    RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /**
   * @brief Peripherals Common Clock Configuration
   * @retval None
   */
-void PeriphCommonClock_Config(void)
-{
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+void PeriphCommonClock_Config(void) {
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-  /** Initializes the peripherals clock
-  */
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CKPER;
-  PeriphClkInitStruct.CkperClockSelection = RCC_CLKPSOURCE_HSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the peripherals clock
+    */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CKPER;
+    PeriphClkInitStruct.CkperClockSelection = RCC_CLKPSOURCE_HSI;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -287,30 +292,29 @@ void PeriphCommonClock_Config(void)
 
 /* MPU Configuration */
 
-void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+void MPU_Config(void) {
+    MPU_Region_InitTypeDef MPU_InitStruct = {0};
 
-  /* Disables the MPU */
-  HAL_MPU_Disable();
+    /* Disables the MPU */
+    HAL_MPU_Disable();
 
-  /** Initializes and configures the Region and the memory to be protected
-  */
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.BaseAddress = 0x0;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
-  MPU_InitStruct.SubRegionDisable = 0x87;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+    /** Initializes and configures the Region and the memory to be protected
+    */
+    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+    MPU_InitStruct.BaseAddress = 0x0;
+    MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+    MPU_InitStruct.SubRegionDisable = 0x87;
+    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+    MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+    MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
 
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-  /* Enables the MPU */
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+    HAL_MPU_ConfigRegion(&MPU_InitStruct);
+    /* Enables the MPU */
+    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 
 }
 
@@ -322,23 +326,21 @@ void MPU_Config(void)
   * @param  htim : TIM handle
   * @retval None
   */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    /* USER CODE BEGIN Callback 0 */
     if (htim->Instance == TIM6) {
         TIM_TypeDef *TIMx;
         static int16_t preCNT[4];
         static int16_t sumX, sumY;
 
-//        // Get position
-//        PMW3901_Read_Data(&CarInfo.pmw);
-//        CarInfo.curX += CarInfo.pmw.deltaX;
-//        CarInfo.curY += CarInfo.pmw.deltaY;
-//
-//        // Get compass
-//        QMC5883_GetData(&CarInfo.hmc);
-//        printf("data:%f,%f,%f\n", CarInfo.hmc.Mx, CarInfo.hmc.My,
-//               ToDig(atan2f(CarInfo.hmc.Mx, CarInfo.hmc.My)));
+        // Get Infrared
+        HC165_Get_Data(&CarInfo.infrared);
+
+        // Get compass
+        QMC5883_GetData(&CarInfo.hmc);
+        CarInfo.yaw = atan2f(CarInfo.hmc.Mx, CarInfo.hmc.My);
+        AngRotate2zero(CarInfo.yaw, CarInfo.initYawOffset);
+//        printf("data:%f,%f,%f\n", CarInfo.hmc.Mx, CarInfo.hmc.My, ToDig(atan2f(CarInfo.hmc.Mx, CarInfo.hmc.My)));
 //
 //        // Get gyro
 //        ICM42605_GetData(&CarInfo.icm, ICM_MODE_ACC | ICM_MODE_GYRO);
@@ -349,6 +351,15 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
         cnt++;
         if (cnt == period) {
             cnt = 0;
+
+            // Get position
+            PMW3901_Read_Data(&CarInfo.pmw);
+            CarInfo.dx = (float)-CarInfo.pmw.deltaX;
+            CarInfo.dy = (float)-CarInfo.pmw.deltaY;
+            VecRotate(CarInfo.dx, CarInfo.dy, CarInfo.yaw);
+            CarInfo.curX += CarInfo.dx;
+            CarInfo.curY += CarInfo.dy;
+
             // Get motor speed
             for (int16_t i = 0; i < 4; i++) {
                 if (i == 0)
@@ -375,13 +386,13 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
             for (int i = 0; i < 4; i++) {
                 uint8_t dir = 1;
 
-                // Optional position loop
-                if (CarInfo.psiCtr) {
+                // Optional position loop 
+                if (CarInfo.mPsiCtr) {
                     CarInfo.spdStep = 0.5f;
-                    float *aimPtr = &CarInfo.mPid[i].ctr.aim;
-                    float res = PID_Realize(&CarInfo.pPid[i], CarInfo.psi[i]);
+                    float *aimPtr = &CarInfo.msPid[i].ctr.aim;
+                    float res = PID_Realize(&CarInfo.mpPid[i], CarInfo.psi[i]);
 
-//                    // Limit accelerate
+                    // Limit accelerate
                     if (res > *aimPtr && res > 0 && *aimPtr >= 0)
                         *aimPtr = *aimPtr < res ? *aimPtr + CarInfo.spdStep : *aimPtr;
                     else if (res < *aimPtr && res < 0 && *aimPtr <= 0)
@@ -389,12 +400,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
                     else *aimPtr = res;
 
                     // Limit speed max
-                    if (CarInfo.mPid[i].ctr.aim > CarInfo.spdLimit)CarInfo.mPid[i].ctr.aim = CarInfo.spdLimit;
-                    if (CarInfo.mPid[i].ctr.aim < -CarInfo.spdLimit)CarInfo.mPid[i].ctr.aim = -CarInfo.spdLimit;
+                    if (CarInfo.msPid[i].ctr.aim > CarInfo.spdLimit)CarInfo.msPid[i].ctr.aim = CarInfo.spdLimit;
+                    if (CarInfo.msPid[i].ctr.aim < -CarInfo.spdLimit)CarInfo.msPid[i].ctr.aim = -CarInfo.spdLimit;
                 }
 
                 // Speed loop
-                float duty = PID_Realize(&CarInfo.mPid[i], CarInfo.spd[i]);
+                float duty = PID_Realize(&CarInfo.msPid[i], CarInfo.spd[i]);
                 if (duty < 0) {
                     duty = -duty;
                     dir = 0;
@@ -412,27 +423,26 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
                 CarInfo.isCarMoving = CarInfo.isCarMoving << 1;
         }
     }
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM17) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
+    /* USER CODE END Callback 0 */
+    if (htim->Instance == TIM17) {
+        HAL_IncTick();
+    }
+    /* USER CODE BEGIN Callback 1 */
 
-  /* USER CODE END Callback 1 */
+    /* USER CODE END Callback 1 */
 }
 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
