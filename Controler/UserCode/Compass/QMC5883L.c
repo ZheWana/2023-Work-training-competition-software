992573d648b21086f8bbf9618f74a8a1295f586e
ZheWana <1312560006@qq.com>
2023-03-24 20:09:28 +0800

    更改校准数据

--- Controler/UserCode/Compass/QMC5883L.c
+++ Controler/UserCode/Compass/QMC5883L.c
@@ -31,9 +31,9 @@ int QMC5883_GetData(hmcData_t *data) {
     data->My = (float) (int16_t) (temp[3] << 8 | temp[2]);
     data->Mz = (float) (int16_t) (temp[5] << 8 | temp[4]);
 
-    data->Mx += -803.3423f;
-//    data->Mx *= 1000.0f / 1700;
-    data->My += -25.2752f;
-//    data->My *= 1000.0f / 1468;
+    data->Mx += -837.058f;
+    data->Mx *= 1000.0f / 1700;
+    data->My += -137.762f;
+    data->My *= 1000.0f / 1468;
     return 0;
 }
